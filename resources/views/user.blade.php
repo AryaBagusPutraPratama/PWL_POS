cf7e36837f97ea48e36e1aa624b2176b758e83aa
AryaBagusPutraPratama <bagusaria14@gmail.com>
2024-03-05 11:28:39 +0700

    Pratikum 2.4-2.5 Jobsheet4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,15 +9,15 @@
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
         {{-- <tr>
+            <th>Jumlah Pengguna</th>
+        </tr> --}}
+        <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
-        </tr> --}}
+        </tr>
         {{-- @foreach ($data as $d)
             <tr>
                 <td>{{ $d->user_id }}</td>
@@ -26,15 +26,15 @@
                 <td>{{ $d->level_id }}</td>
             </tr>
         @endforeach --}}
-        {{-- <tr>
+        <tr>
             <td>{{ $data->user_id }}</td>
             <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
-        </tr> --}}
-        <tr>
-            <td>{{ $data }}</td>
         </tr>
+        {{-- <tr>
+            <td>{{ $data }}</td>
+        </tr> --}}
     </table>
 </body>
 </html>
