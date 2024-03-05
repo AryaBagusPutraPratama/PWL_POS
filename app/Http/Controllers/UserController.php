13b350ac9e0c5de56d662582645e5c1ef63a7ec2
AryaBagusPutraPratama <bagusaria14@gmail.com>
2024-03-05 09:12:10 +0700

    Pratikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,13 +11,21 @@ class UserController extends Controller
 {
     public function index() {
         // tambah data user dengan Eloquent Model
-        $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 5
+        // ];
+        // UserModel::insert($data); // tambahkan data ke tabel
+
+        $data =[
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
             'password' => Hash::make('12345'),
-            'level_id' => 5
         ];
-        UserModel::insert($data); // tambahkan data ke tabel
+        UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
